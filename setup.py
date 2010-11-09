02441df17912e9b64c8e053e2f04c145cd6d8808
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2010-11-09 16:28:25 +0000

    Update README and version 1.0 in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='django-jira',
-      version='0.1',
+      version='1.0',
       description='An automated exception reporter to JIRA from Django',
       author='Chris Northwood',
       author_email='christopher.northwood@oucs.ox.ac.uk',
